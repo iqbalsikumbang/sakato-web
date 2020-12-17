9b769825acb48ca8ca005460afec1b69c73958fc
IqbalSikumbang <iqbalsikumbang767@gmail.com>
2020-12-17 16:19:56 +0700

    first commit

---
+++ dbh.php
@@ -0,0 +1,10 @@
+<?php
+
+$host = "localhost";
+$username = "u412789544_sadb";
+$password = "082133168653";
+$dbname = "u412789544_sakatodb";
+
+$conn = mysqli_connect($host, $username, $password, $dbname);
+
+?>
